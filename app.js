6e532f0691cf05f2331d153d47828842e48fe6b3
Jinxiu.Chen <chenjx1024@outlook.com>
2022-06-23 16:51:41 +1000

    build: add the babel-loader

---
+++ app.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div> This is the first react component</div>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   mode: "development",
   entry: "./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 };
